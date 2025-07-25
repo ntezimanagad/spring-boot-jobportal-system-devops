69bfde357bca87a8d4f2559a96a62bcadb5db47b
ntezimanagad <ntezimanagad4@gmail.com>
2025-07-25 20:46:47 +0200

    Initial commit

--- src/main/java/com/learn/chatapp/model/User.java
+++ src/main/java/com/learn/chatapp/model/User.java
@@ -17,6 +17,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Table(name = "\"user\"")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
